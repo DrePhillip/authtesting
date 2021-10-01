9b95f86188e6ca80d6e178ac75f02f20c579e2ff
DrePhillip <dreduarte72@gmail.com>
2021-10-01 18:51:04 +0000

    Add lvl 5 security - Using sessions and cookies

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require("mongoose");
-//require hash method using bcrypt
-const bcrypt = require("bcrypt");
-//Add salt rounds to make the breach even more difficult
-const saltRounds = 12;
+//Adding session dependencies
+const session = require ("express-session");
+const passport = require ("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const port = 3000;
 
 const app = express();
@@ -17,6 +18,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+// Use session here before mongoose connection
+app.use(session({
+  secret:"Our little secret.",
+  resave: false,
+  saveUninitialized:false
+}));
+//use passport and sessoin
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 mongoose.connect('mongodb://localhost:27017/userDB');
 
@@ -25,9 +36,16 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-
+//Hash and salt Passwords
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+//Create strategy
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -35,52 +53,52 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login");
 });
+app.get("/secrets", (req,res)=>{
+  //If the user is authenticated, then it will render the dashboard
+  //Else it will redirect to login
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  }else{
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout" , (req,res)=>{
+  //logout and end session
+  req.logout();
+  res.redirect("/");
+})
 app.get("/register", (req, res) => {
   res.render("register");
 });
 app.post("/register", (req, res) => {
-  //Begin salting
-  bcrypt.genSalt(saltRounds, function(err, salt) {
-    bcrypt.hash(req.body.password, salt, function(err, hash) {
-      //REGISTER USER USING MONGODB
-      const newUser = new User({
-        email: req.body.username,
-        //register hashed password after the salt rounds
-        password: hash
+  //REGISTER USER USING PASSPORT
+  User.register({username:req.body.username}, req.body.password, function(err,user){
+    if (err){
+      console.log(err);
+      res.redirect("/register");
+    }else {
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets");
       });
-
-      newUser.save(function(err) {
-        if (err) {
-          console.log(err);
-        } else {
-          res.render("secrets");
-        }
-      });
-    });
+    }
   });
 });
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  //Hashed pw check
-  const password = req.body.password;
-  //CHECK IF USER LOGIN MATCHES THE SAME LOGIN ON THE DB
-  User.findOne({
-    email: username
-  }, function(err, foundUser) {
-    if (err) {
+  const user = new User({
+    username:req.body.username,
+    password:req.body.password
+  });
+//use login option from passport to check data and start session if
+//correct
+  req.login(user,function(err){
+    if (err){
       console.log(err);
     } else {
-      if (foundUser) {
-        // Load hash from your password DB.
-        bcrypt.compare(password, hash, function(err, result) {
-          //If password matches with the database, then it renders
-          //the dashboard
-          if (result === true){
-          res.render("secrets");
-        }
-        });
-      }
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets");
+      });
     }
   });
 });
